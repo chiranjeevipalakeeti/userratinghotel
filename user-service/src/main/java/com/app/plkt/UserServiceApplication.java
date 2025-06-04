1c9ce5ec1c0b3b96e401abbbcd41292da981dd73
chiranjeevi1 <chandu.143326@gmail.com>
2025-06-04 11:57:06 +0530

    config server

--- user-service/src/main/java/com/app/plkt/UserServiceApplication.java
+++ user-service/src/main/java/com/app/plkt/UserServiceApplication.java
@@ -2,12 +2,14 @@ package com.app.plkt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate()
 	{
 		return  new RestTemplate();
